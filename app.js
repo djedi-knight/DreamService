16d6060aa6a98205fdd5511ae6864f4848fb44b3
Shawn Daichendt <sdaichendt@alterisgroup.com>
2017-10-31 14:58:30 -0400

    created initial index html page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 var redis = require('redis');
+var path = require('path');
 
 // create a new Redis client and connect to Redis Labs instance
 var client = redis.createClient({
@@ -12,8 +13,15 @@ client.on('error', function (err) {
   console.log("Error " + err);
 });
 
+// setup server API
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+// set port
 app.set('port', (process.env.PORT || 5000));
 
+// start server
 app.listen(app.get('port'), function(){
   console.log('Server listening on port: ', app.get('port'));
 });
