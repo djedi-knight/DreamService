1d5fd91903fe10724c944f417e5c06c0f14e9085
Shawn Daichendt <sdaichendt@alterisgroup.com>
2017-11-01 10:22:21 -0400

    finished /add API server method

--- app.js
+++ app.js
@@ -1,38 +1,58 @@
+// Declare inputs
 var app = require('express')();
 var redis = require('redis');
 var bodyParser = require('body-parser');
 var path = require('path');
 
-// create a new Redis client and connect to Redis Labs instance
+// Create a new Redis client and connect to Redis Labs instance
 var client = redis.createClient({
   host: 'redis-13348.c15.us-east-1-2.ec2.cloud.redislabs.com',
   port: '13348'
 });
 
-// if an error occurs, print it to the console
+// If an error occurs, print it to the console
 client.on('error', function (err) {
   console.log("Error " + err);
 });
 
-// initial setup of game state
+// Initial setup of game state
 client.set('count', 0);
 
-client.get('count', function (err, reply) {
-  console.log(reply.toString());
-});
-
-// set server to support JSON-encoded bodies
+// Set server to support JSON-encoded bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// setup server API
+// Setup server API
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 app.post('/add', function (req, res) {
-  console.log('in POST /add');
-  console.log('req.body', req.body);
+  // Get value from request body
+  let value = parseInt(req.body.value || 0);
+
+  // Get game state
+  client.get('count', function (err, currentCount) {
+    if (err) {
+      throw(err);
+    }
+
+    // Add value to game state
+    let updatedCount = parseInt(currentCount) + value;
+
+    // Set updated game state
+    client.set('count', updatedCount);
+
+    // Get new game state
+    client.get('count', function (err, newCount) {
+      if (err) {
+        throw(err);
+      }
+
+      // Return result
+      res.send({result: newCount});
+    });
+  });
 });
 
 app.post('/sub', function (req, res) {
@@ -50,10 +70,10 @@ app.post('/show', function (req, res) {
   console.log('req.body', req.body);
 });
 
-// set port
+// Set port
 app.set('port', (process.env.PORT || 5000));
 
-// start server
+// Start server
 app.listen(app.get('port'), function(){
   console.log('Server listening on port: ', app.get('port'));
 });
